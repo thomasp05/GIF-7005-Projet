7c5eecf86bdef67c51e8e64bd90777312e6228c5
igorpontes <ipontes@addenergie.ca>
2020-11-17 23:59:16 -0500

    Add ResNetUNet model

--- models_parts.py
+++ models_parts.py
@@ -3,8 +3,12 @@ import torch.nn as nn
 
 def double_conv(in_channels, out_channels):
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, 3, padding=1),
-        nn.ReLU(inplace=True)
+        conv_relu(in_channels, out_channels, 3, 1),
+        conv_relu(in_channels, out_channels, 3, 1)
     )
+
+def conv_relu(in_channels, out_channels, kernel, padding):
+  return nn.Sequential(
+    nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+    nn.ReLU(inplace=True)
+  )
