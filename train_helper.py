34185accbe97733e6dd769e39f3e61f239b9a055
igorpontes <ipontes@addenergie.ca>
2020-11-22 18:11:27 -0500

    add functions to plot images

--- train_helper.py
+++ train_helper.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import time
+import matplotlib.pyplot as plt
+import numpy as np
 from collections import defaultdict
+from functools import reduce
 
 
 # Source: A survey of loss functions for semantic segmentation https://arxiv.org/pdf/2006.14822.pdf
@@ -104,3 +107,18 @@ def train_model(model, optimizer, scheduler, data_loaders, num_epochs = 10, chec
     # load best model weights
     model.load_state_dict(torch.load(checkpoint_path))
     return model
+
+
+def plot_img_array(img_array, ncol=3):
+    nrow = len(img_array) // ncol
+
+    f, plots = plt.subplots(nrow, ncol, sharex='all', sharey='all', figsize=(ncol * 4, nrow * 4))
+
+    for i in range(len(img_array)):
+        plots[i // ncol, i % ncol]
+        plots[i // ncol, i % ncol].imshow(img_array[i])
+
+
+def plot_side_by_side(img_arrays):
+    flatten_list = reduce(lambda x,y: x+y, zip(*img_arrays))
+    plot_img_array(np.array(flatten_list), ncol=len(img_arrays))
