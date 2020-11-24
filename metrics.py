088028844e375c370cb221ad684ff32cfeebc71b
Ludovic Girard <ludovic.girard@hotmail.com>
2020-11-24 14:37:27 -0500

    Change interface to reduce memory demand

--- metrics.py
+++ metrics.py
@@ -13,48 +13,83 @@ def run_test(model, dataloader, tests, device):
         labels = labels.to(device)
         bounding_box = bounding_box.to(device)
 
-        bounding_box_pred_ = model(img)
+        bounding_box_pred = model(img)
+        bounding_box_pred[bounding_box_pred > 0.5] = 1
+        bounding_box_pred[bounding_box_pred < 0.5] = 0
 
-        if i == 0:
-            # Pre-allocate results arrays
-            real_bounding_box = torch.zeros(
-                (len(dataloader.dataset), 1,
-                 bounding_box.shape[-2], bounding_box.shape[-1])).to(device)
-            pred_bounding_box = torch.zeros(real_bounding_box.shape).to(device)
-
-            real_labels = torch.zeros((len(dataloader.dataset))).to(device)
-            pred_labels = torch.zeros(real_labels.shape).to(device)
-
-        real_bounding_box[img_processed:img.shape[0]] = bounding_box
-        pred_bounding_box[img_processed:img.shape[0]] = bounding_box_pred_
-        real_labels[img_processed:img.shape[0]] = labels
-        pred_labels[img_processed:img.shape[0]
-                    ] = bounding_box_pred_.flatten(-2, -1).max(-1).values.squeeze()
+        pred_labels = bounding_box_pred.flatten(
+            -2, -1).max(-1).values.squeeze()
 
         for test in tests:
 
-            test(real_bounding_box, pred_bounding_box, real_labels, pred_labels)
+            test(bounding_box, bounding_box_pred, labels, pred_labels)
+
+    for test in tests:
+
+        test.compute()
 
 
-def IoU(bounding_box_1, bounding_box_2, *args):
+class IoU:
 
-    intersection = torch.logical_and(bounding_box_1, bounding_box_2).sum()
-    union = torch.logical_or(bounding_box_1, bounding_box_2).sum()
+    def __init__(self):
 
-    print("IoU : {}".format(100 * intersection.cpu() / union.cpu()))
+        self.iou = []
+        self.weights = []
+        self.result = None
+
+    def __call__(self, bb1, bb2, *args):
+
+        intersection = torch.logical_and(
+            bb1.detach(), bb2.detach()).sum()
+        union = torch.logical_or(bb1.detach(),
+                                 bb2.detach()).sum()
+
+        self.iou.append(100 * intersection.cpu() / union.cpu())
+        self.weights.append(bb1.shape[0])
+
+    def __str__(self):
+
+        print(self.result)
+
+    def compute(self):
+
+        self.result = (torch.tensor(self.iou) * torch.tensor(self.weights)
+                       ).sum() / torch.tensor(self.weights).sum()
+
+        print("IoU : {} %".format(self.result))
 
 
-def confusion_matrix(bb1, bb2, real_labels, pred_labels):
+class Confusion_matrix:
 
-    mat = torch.zeros((2, 2))
+    def __init__(self):
 
-    pred_labels[pred_labels > 0.5] = 1
-    pred_labels[pred_labels < 0.5] = 0
+        self.mat = None
+        self.result = None
 
-    for i in range(2):
-        for j in range(2):
-            mat[i, j] = torch.logical_and(
-                (pred_labels.flatten() == i), (real_labels.flatten() == j)).sum()
+    def __call__(self, bb1, bb2, real_labels, pred_labels):
 
-    print("Confusion matrix:")
-    print(mat)
+        mat_ = torch.zeros((2, 2))
+
+        pred_labels[pred_labels > 0.5] = 1
+        pred_labels[pred_labels < 0.5] = 0
+
+        for i in range(2):
+            for j in range(2):
+                mat_[i, j] = torch.logical_and(
+                    (pred_labels.flatten() == i), (real_labels.flatten() == j)).sum()
+
+        if self.mat == None:
+            self.mat = mat_.unsqueeze(0).cpu()
+
+        else:
+            self.mat = torch.cat([self.mat, mat_.unsqueeze(0).cpu()], 0)
+
+    def __str__(self):
+
+        print(self.result)
+
+    def compute(self):
+
+        self.result = self.mat.sum(0)
+
+        print(self.result)
